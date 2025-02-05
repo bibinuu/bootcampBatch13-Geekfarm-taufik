b234c5a41c2e71d9f2a6a3c5bfbf0afe2230c065
Mohamad-Taufik-Rizki-29 <bibinuu29@gmail.com>
2025-02-05 12:26:03 +0700

    membuat folder file penyimpanan jika tidak ada

--- readline.js
+++ readline.js
@@ -1,17 +1,45 @@
 
+const validator = require('validator');
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
-
+const fs = require('fs');
+// const { mkdir } = require('node:fs');
 const rl = readline.createInterface({ input, output });
+
+if (!fs.existsSync('data/contacts.json')){
+    fs.mkdirSync("data");
+    fs.writeFileSync("data/contacts.json", '[]');
+} 
 
 rl.question('What is your name? ', (name) => {
     rl.question('Whar is your phone number? ', (number) => {
         rl.question('What is your email? ', (email) => {
+            const valNumber = validator.isMobilePhone(number,['id-ID']);
+            const data = {name, number, email};  
+            const file = fs.readFileSync("data/contacts.json", "utf-8")
+            const contacts = JSON.parse(file);
+            
             console.log(`Your name ${name}`);
-            console.log(`Your phone number ${number}`);
-            console.log(`Your email ${email}`);
+            if (valNumber) {
+                console.log(`Your number ${number}`);
+            } else {
+                console.log('Your number is false');
+                
+            }
+            const valEmail = validator.isEmail(email);
+            if (valEmail) {
+                console.log(`Your number ${email}`);
+            } else {
+                console.log('Your email is false');
+            }
+
+
+        
+            contacts.push(data);
+            fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+
+
             rl.close();
           });
-
       });
 });
